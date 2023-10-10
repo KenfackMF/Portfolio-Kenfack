4687ece37e8dda561b71bcb5ddb1da67fcdb2735
KenfackMF <francis.kenfack@gmail.com>
2023-10-10 22:25:55 +0200

    correction marge ok

--- src/pages/APropos.js
+++ src/pages/APropos.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHtml5, faCss3, faJsSquare, faSass, faNode } from "@fortawesome/free-brands-svg-icons"; // Icônes de marques
+import { faDatabase } from "@fortawesome/free-solid-svg-icons"; // Icône solide
+
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "../styles/propos.scss";
-import Skills from "../components/Skills.js";
 import image from "../assets/image_3.jpg";
-import { about, section2title, skillsBar } from "../data/profile.js";
+import { about, section2title } from "../data/profile.js";
 
 const APropos = () => {
   // Fonction pour ouvrir un lien dans une nouvelle fenêtre
@@ -14,9 +17,9 @@ const APropos = () => {
   return (
     <div>
       <a id="Apropos"></a>,
-      <div className="block-apropos">
+      <div className="block-apropos-1">
         <h2 className="propos-titre">{section2title}</h2>
-        <div className="bloc-propos">
+        <div className="block-apropos-2">
           <div className="block-gauche">
             <i className="bi bi-award-fill animate-icon"></i>
             <p className="texte-propos">{about.paragraph}</p>
@@ -36,10 +39,31 @@ const APropos = () => {
           </div>
         </div>
         <div id="Skills">
-          <div className="row d-flex justify-content-center skills">
-            {skillsBar.map((x) => (
-              <Skills key={x.name} faClass={x.faClass} svg={x.svg} label={x.name} />
-            ))}
+          <div className="skills">
+            <div className="icone">
+              <FontAwesomeIcon icon={faHtml5} />
+              <p>HTML</p>
+            </div>
+            <div className="icone">
+              <FontAwesomeIcon icon={faCss3} />
+              <p>CSS</p>
+            </div>
+            <div className="icone">
+              <FontAwesomeIcon icon={faJsSquare} />
+              <p>JavaScript</p>
+            </div>
+            <div className="icone">
+              <FontAwesomeIcon icon={faSass} />
+              <p>Sass</p>
+            </div>
+            <div className="icone">
+              <FontAwesomeIcon icon={faNode} />
+              <p>Node</p>
+            </div>
+            <div className="icone">
+              <FontAwesomeIcon icon={faDatabase} />
+              <p>Database</p>
+            </div>
           </div>
         </div>
       </div>
